563e64fb29d0bd0aee2eabd73fac025c60c2462e
Fellipe Castro <contact@fellipecastro.com>
2021-03-11 14:29:21 -0300

    Update

--- src/main/java/io/github/fellipecastro/Config.java
+++ src/main/java/io/github/fellipecastro/Config.java
@@ -1,10 +1,15 @@
 package io.github.fellipecastro;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class Config {
-    @Bean(name = "applicationName")
-    public String applicationName() { return "Sistema de Vendas"; }
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+    }
 }
