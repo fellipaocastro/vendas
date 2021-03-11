ad327fe446d53fb93ae10eea5bc276521deea8af
Fellipe Castro <contact@fellipecastro.com>
2021-03-11 14:59:15 -0300

    Update

---
+++ src/main/java/io/github/fellipecastro/Animal.java
@@ -0,0 +1,5 @@
+package io.github.fellipecastro;
+
+public interface Animal {
+    void fazerBarulho();
+}
--- src/main/java/io/github/fellipecastro/Config.java
+++ src/main/java/io/github/fellipecastro/Config.java
@@ -2,11 +2,8 @@ package io.github.fellipecastro;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("development")
+@Development
 public class Config {
     @Bean
     public CommandLineRunner executar(){
